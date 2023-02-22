e0ff53189c98eb626c5ac41e0067ed788bea839c
kaushik1904 <kaushikprajapati1970@gmail.com>
2023-02-22 14:21:45 +0530

    task done

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,10 +1,100 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent {
+  public userForm: any;
+  public userData: any = [];
 
+  ngOnInit() {}
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.userForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      middleName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      textArea: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      hobby: this.fb.group({
+        cricket: [''],
+        dancing: [''],
+      }),
+      company: ['', [Validators.required]],
+    });
+  }
+
+  public company = [
+    'Tech Extensor',
+    'Google',
+    'Amazon',
+    'Flipcart',
+    'Uber',
+    'Zomato',
+    'IBM',
+    'TCS',
+  ];
+
+  get firstName() {
+    return this.userForm.controls['firstName'];
+  }
+  get middleName() {
+    return this.userForm.controls['middleName'];
+  }
+  get lastName() {
+    return this.userForm.controls['lastName'];
+  }
+
+  onSubmit() {
+    const user = this.userForm.value;
+    const tmp = [user];
+    localStorage.setItem(
+      `${this.route.snapshot.params['id']}`,
+      JSON.stringify(tmp)
+    );
+  }
+
+  goBack() {
+    this.router.navigate(['home']);
+  }
+
+  clearAll() {
+    this.userForm.setValue({
+      firstName: '',
+      middleName: '',
+      lastName: '',
+      textArea: '',
+      age: '',
+      gender: '',
+      hobby: {
+        cricket: '',
+        dancing: '',
+      },
+      company: '',
+    });
+  }
+  
+  fetchData() {
+    this.userData = JSON.parse(
+      localStorage.getItem(this.route.snapshot.params['id']) || '{}'
+    );
+    this.userForm.setValue({
+      firstName: [this.userData[0]['firstName']],
+      middleName: [this.userData[0]['middleName']],
+      lastName: [this.userData[0]['lastName']],
+      textArea: [this.userData[0]['textArea']],
+      age: [this.userData[0]['age']],
+      gender: this.userData[0]['gender'],
+      hobby: this.userData[0]['hobby'],
+      company: [this.userData[0]['company']],
+    });
+  }
 }
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../user';
 
 @Component({
@@ -7,6 +8,38 @@ import { User } from '../user';
   styleUrls: ['./template.component.css'],
 })
 export class TemplateComponent {
+  constructor(private route: Router, private router: ActivatedRoute) {}
+
+  ngOnInit() {
+    if (localStorage.getItem(this.router.snapshot.params['id']) != null) {
+      this.userData = JSON.parse(
+        localStorage.getItem(this.router.snapshot.params['id']) || '{}'
+      );
+
+      this.userModel = new User(
+        this.userData[0]['firstName'],
+        this.userData[0]['middleName'],
+        this.userData[0]['lastName'],
+        this.userData[0]['textArea'],
+        this.userData[0]['age'],
+        this.userData[0]['gender'],
+        this.userData[0]['hobby'],
+        this.userData[0]['company']
+      );
+    }
+  }
+
+  public company = [
+    'Tech Extensor',
+    'Google',
+    'Amazon',
+    'Flipcart',
+    'Uber',
+    'Zomato',
+    'IBM',
+    'TCS',
+  ];
+
   userModel = new User(
     '',
     '',
@@ -17,4 +50,52 @@ export class TemplateComponent {
     { cricket: '', dancing: '' },
     ''
   );
+
+  public id: string = '';
+  public userData: any = [];
+  goBack() {
+    this.route.navigate(['home']);
+  }
+
+  clearAll() {
+    this.userModel.firstName = '';
+    this.userModel.middleName = '';
+    this.userModel.lastName = '';
+    this.userModel.textArea = '';
+    this.userModel.age = '';
+    this.userModel.gender = '';
+    this.userModel.hobby.cricket = '';
+    this.userModel.hobby.dancing = '';
+    this.userModel.company = '';
+  }
+
+  onSubmit() {
+    this.userModel.textArea =
+      this.userModel.firstName +
+      ' ' +
+      this.userModel.middleName +
+      ' ' +
+      this.userModel.lastName;
+
+    this.id = this.router.snapshot.params['id'];
+    console.log(this.id);
+    console.log(this.userModel);
+    this.userData.push({
+      id: this.id,
+      firstName: this.userModel.firstName,
+      middleName: this.userModel.middleName,
+      lastName: this.userModel.lastName,
+      textArea: this.userModel.textArea,
+      age: this.userModel.age,
+      gender: this.userModel.gender,
+      hobby: {
+        cricket: this.userModel.hobby.cricket,
+        dancing: this.userModel.hobby.dancing,
+      },
+      company: this.userModel.company,
+    });
+    console.log(this.userData);
+    localStorage.setItem(this.id, JSON.stringify(this.userData));
+    this.route.navigate(['reactive/' + this.id]);
+  }
 }
